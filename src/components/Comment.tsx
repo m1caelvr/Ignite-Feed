be64105ef2a4d7d6c7c1c67522de07c857f25ada
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-08-15 17:31:15 -0300

    Comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+
+import { Avatar } from './Avatar'
+import { useState } from 'react'
+
+import styles from './Comment.module.css'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() { 
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=50&w=100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Micael Vitor</strong>
+                            <time
+                                title="14 de agosto de 2024"
+                                dateTime="2024-08-14" >
+                                    Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir<span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
